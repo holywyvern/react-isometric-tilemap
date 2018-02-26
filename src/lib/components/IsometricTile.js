5c83b7901c66f4aa58b97949a75d317359be10ac
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2018-02-26 02:06:15 -0300

    Added images as middle walls

--- src/lib/components/IsometricTile.js
+++ src/lib/components/IsometricTile.js
@@ -31,11 +31,27 @@ class IsometricTile extends Component {
   static defaultProps = {
     z: 0,
     leftZ: null,
-    rightZ: null
+    rightZ: null,
+    textures: null
   };
 
+  getMiddleWalls(textures, height, prefix) {
+    const result = [];
+    for (let i = 1; i < height; ++i) {
+      result.push(
+        <img
+          key={`${prefix}-middle-wall`}
+          src={textures.middle}
+          alt=""
+          style={{ "--wall-index": i }}
+        />
+      );
+    }
+    return result;
+  }
+
   render() {
-    const { x, y, z, leftZ, rightZ } = this.props;
+    const { x, y, z, leftZ, rightZ, textures } = this.props;
     const lz = leftZ === null ? z : leftZ;
     const rz = rightZ === null ? z : rightZ;
     const vars = {
@@ -50,6 +66,25 @@ class IsometricTile extends Component {
         <div className="floor" />
         {lz > 0 ? <div className="wall left" /> : null}
         {rz > 0 ? <div className="wall right" /> : null}
+        <div className="textures">
+          {textures && textures.floor ? (
+            <img src={textures.floor} alt="" className="floor" />
+          ) : null}
+          {textures && textures.leftWall && lz > 0
+            ? [
+                <img key="l-top" alt="" src={textures.leftWall.top} />,
+                <img key="l-bottom" alt="" src={textures.leftWall.bottom} />
+              ]
+            : null}
+          {textures && textures.rightWall && lz > 0
+            ? [
+                <img key="r-top" alt="" src={textures.rightWall.top} />,
+                <img key="r-bottom" alt="" src={textures.rightWall.bottom} />
+              ]
+            : null}
+          {this.getMiddleWalls(textures && textures.rightWall, lz, "l")}
+          {this.getMiddleWalls(textures && textures.leftWall, rz, "l")}
+        </div>
       </div>
     );
   }
